c661c96ef01d6744a582036a93ec37fff3fac852
Prince-of-sea@KMR <94970975+Prince-of-sea@users.noreply.github.com>
2025-03-04 00:01:18 +0900

    ‍🔧 連番動画無効化時変数修正2

--- src/ons_script.py
+++ src/ons_script.py
@@ -359,7 +359,7 @@ mov %multiconverteralias5,%5 :mov %multiconverteralias6,%6 :mov %multiconvertera
 mov $multiconverteralias0,$10:mov $multiconverteralias1,$11:mov $multiconverteralias2,$12:mov $multiconverteralias3,$13:mov $multiconverteralias4,$14:mov $multiconverteralias5,$15
 getparam $13,%9:fileexist %0,$13+"/00000.jpg"
 fileexist %1,"_DISABLED_VIDEO"
-if %1==1 mov $13,$multiconverteralias3:mov %9,%multiconverteralias9:mov %1,%multiconverteralias1:return
+if %1==1 mov $13,$multiconverteralias3:mov %9,%multiconverteralias9:mov %1,%multiconverteralias1:saveon:return
 if %0==1 mov $14,".jpg"
 if %0!=1 mov $14,".png"
 mov $11,$13:add $11,"/":mov %6,5:mov $15,""
